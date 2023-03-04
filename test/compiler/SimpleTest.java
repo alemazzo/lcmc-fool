7ede38e9979d3396774b5e73e0ffa8754f185dd9
Alessandro <alemazzoli97@gmail.com>
2023-03-04 11:01:13 +0100

    Refactored and introduced first tests

--- test/compiler/SimpleTest.java
+++ test/compiler/SimpleTest.java
@@ -1,18 +1,40 @@
 package compiler;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SimpleTest {
 
-    private final int index = 0;
+    private final String fileName = "test/prova.fool";
+    private FOOLLexer lexer;
+    private FOOLParser parser;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        final CharStream chars = CharStreams.fromFileName(fileName);
+        lexer = new FOOLLexer(chars);
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        parser = new FOOLParser(tokens);
+    }
+
 
     @Test
-    void testInitialValueToBeZero() {
-        assertEquals(0, index);
+    void testSimpleParsingShouldHaveZeroLexicalErrors() {
+        parser.prog();
+        assertEquals(0, lexer.lexicalErrors);
+    }
+
+    @Test
+    void testSimpleParsingShouldHaveZeroSyntaxErrors() {
+        parser.prog();
+        assertEquals(0, parser.getNumberOfSyntaxErrors());
     }
 
 }
